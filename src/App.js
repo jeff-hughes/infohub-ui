455a83e42c7034c9748c3e662fd0e0ec96eba0d0
Jeff Hughes <jeff.hughes@gmail.com>
2020-01-18 13:16:03 -0500

    Implement basic news feed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+var json = {
+    'news': [
+        { 'source': 'ESPN', 'title': 'Sportsball champions win the sportsball tournament again!', 'text': "I don't understand teh sports" },
+        { 'source': 'Hacker News', 'title': '1337 haX0rs are 1337', 'text': "Turns out I don't understand hacking either" },
+        { 'source': 'Generic News', 'title': 'Things happened again today', 'text': "Some things happened. Then other things happened as well. It was an exciting occasion." }
+    ]
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <NewsFeed items={json.news} />
+        </div>
+    );
+}
+
+function NewsFeed(props) {
+    var listItems = props.items.map((item) =>
+        <NewsItem source={item.source} title={item.title} text={item.text} />
+    );
+    return <ul className="NewsFeed">{listItems}</ul>;
+}
+
+function NewsItem(props) {
+    return (
+        <li className="NewsItem">
+            <div>
+                <h3 className="title">{props.title}</h3>
+                <p className="source">{props.source}</p>
+                <p className="text">{props.text}</p>
+            </div>
+        </li>
+    );
 }
 
 export default App;
