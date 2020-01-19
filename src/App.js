165b0854c79ef3a945e159b5ac358cb4142eb019
Jeff Hughes <jeff.hughes@gmail.com>
2020-01-19 12:38:31 -0500

    Implement simple dummy AJAX request

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+
+const FETCH_DELAY = 5;  // delay in seconds between fetching news items
 
 var json = {
     'news': [
         { 'source': 'ESPN', 'title': 'Sportsball champions win the sportsball tournament again!', 'text': "I don't understand teh sports" },
-        { 'source': 'Hacker News', 'title': '1337 haX0rs are 1337', 'text': "Turns out I don't understand hacking either" },
-        { 'source': 'Generic News', 'title': 'Things happened again today', 'text': "Some things happened. Then other things happened as well. It was an exciting occasion." }
+        { 'source': 'Hacker News', 'title': '1337 haX0rs are 1337', 'text': "Turns out I don't understand hacking either" }
     ]
+}
+
+function dummyAJAXResponse(userId, timestamp) {
+    return [{ 'source': 'Generic News', 'title': 'Things happened again today', 'text': "Some things happened. Then other things happened as well. It was an exciting occasion." }];
 }
 
 function App() {
     return (
         <div className="App">
-            <NewsFeed items={json.news} />
+            <NewsFeed />
         </div>
     );
 }
 
-function NewsFeed(props) {
-    var listItems = props.items.map((item) =>
+function NewsFeed() {
+    const [items, setItems] = useState(json.news);
+    var listItems = items.map((item) =>
         <NewsItem source={item.source} title={item.title} text={item.text} />
     );
+
+    setInterval(() => { 
+        var newItems = dummyAJAXResponse(null, null);
+        setItems(newItems.concat(items));
+    }, FETCH_DELAY*1000);
+
     return <ul className="NewsFeed">{listItems}</ul>;
 }
 
