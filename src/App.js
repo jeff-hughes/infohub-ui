519aef568f88fc6c63ddfb37a6a610c298cec0ec
Jeff Hughes <jeff.hughes@gmail.com>
2020-01-20 11:25:02 -0500

    Change NewsFeed from function to class for proper AJAX

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 
 const FETCH_DELAY = 5;  // delay in seconds between fetching news items
+var test_id_counter = 3; // incremented with each call of dummyAJAXResponse()
 
 var json = {
     'news': [
-        { 'source': 'ESPN', 'title': 'Sportsball champions win the sportsball tournament again!', 'text': "I don't understand teh sports" },
-        { 'source': 'Hacker News', 'title': '1337 haX0rs are 1337', 'text': "Turns out I don't understand hacking either" }
+        { 'id': 1, 'source': 'ESPN', 'title': 'Sportsball champions win the sportsball tournament again!', 'text': "I don't understand teh sports" },
+        { 'id': 2, 'source': 'Hacker News', 'title': '1337 haX0rs are 1337', 'text': "Turns out I don't understand hacking either" }
     ]
 }
 
 function dummyAJAXResponse(userId, timestamp) {
-    return [{ 'source': 'Generic News', 'title': 'Things happened again today', 'text': "Some things happened. Then other things happened as well. It was an exciting occasion." }];
+    var item = [{ 'id': test_id_counter, 'source': 'Generic News', 'title': 'Things happened again today', 'text': "Some things happened. Then other things happened as well. It was an exciting occasion." }];
+    test_id_counter++;
+    return item;
 }
 
 function App() {
@@ -23,18 +26,35 @@ function App() {
     );
 }
 
-function NewsFeed() {
-    const [items, setItems] = useState(json.news);
-    var listItems = items.map((item) =>
-        <NewsItem source={item.source} title={item.title} text={item.text} />
-    );
+class NewsFeed extends React.Component {
+    constructor(props) {
+        super(props);
 
-    setInterval(() => { 
-        var newItems = dummyAJAXResponse(null, null);
-        setItems(newItems.concat(items));
-    }, FETCH_DELAY*1000);
+        // putting this here for now, but eventually this will
+        // be pulled from an AJAX request after component has
+        // mounted
+        this.state = {
+            items: json.news
+        };
+    }
 
-    return <ul className="NewsFeed">{listItems}</ul>;
+    componentDidMount() {
+        setInterval(() => {
+            var newItems = dummyAJAXResponse(null, null);
+            this.setState(state => {
+                return { items: state.items.concat(newItems) };
+            });
+        }, FETCH_DELAY*1000);
+    }
+
+    render() {
+        var listItems = this.state.items.slice()  // shallow copy of array
+            .sort((a, b) => b - a)                // sort descending
+            .map((item) =>
+                <NewsItem key={item.id} source={item.source} title={item.title} text={item.text} />
+            );
+        return <ul className="NewsFeed">{listItems}</ul>;
+    }
 }
 
 function NewsItem(props) {
